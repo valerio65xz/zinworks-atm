ca1e820fdb0d160f120c600c7a992dbb0f0d6a57
vale.bond <vale-bond@hotmail.it>
2022-07-29 15:18:22 +0200

    create project

---
+++ src/test/java/com/zinworks/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinworks.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
